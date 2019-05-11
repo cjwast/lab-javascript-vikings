75f1216ea095539a3b89d3add906589bb145308b
Charlie Aguirre <cj.wast@hotmail.com>
2019-05-10 21:11:59 -0500

    final commit :zap:

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -65,10 +65,51 @@ class War {
   }
 
   vikingAttack(){
-    this.saxonArmy.forEach()
+    let vikingPosition = getRandomInt(this.vikingArmy);
+    let saxonPosition = getRandomInt(this.saxonArmy);
+
+    let vikingStrength = this.vikingArmy[vikingPosition].attack();
+    let result = this.saxonArmy[saxonPosition].receiveDamage(vikingStrength);
+
+    console.log(result);
+
+    if (this.saxonArmy[saxonPosition].health <= 0) {
+      this.saxonArmy.splice(saxonPosition,1);
+    }
+
+    return result;
+  }
+
+  saxonAttack(){
+    let vikingPosition = getRandomInt(this.vikingArmy);
+    let saxonPosition = getRandomInt(this.saxonArmy);
+
+    let saxonStrength = this.saxonArmy[saxonPosition].attack();
+    let result = this.vikingArmy[vikingPosition].receiveDamage(saxonStrength);
+
+    console.log(result);
+
+    if (this.vikingArmy[vikingPosition].health <= 0) {
+      this.vikingArmy.splice(vikingPosition,1);
+    }
+
+    return result;
+  }
+
+  showStatus(){
+    if (this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survive another day...";
+    } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+      return "Vikings and Saxons are still in the thick of battle."
+    }
   }
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+function getRandomInt(array = []) {
+  return Math.floor(Math.random() * (array.length - 0)) + 0;
 }
+
+
+
